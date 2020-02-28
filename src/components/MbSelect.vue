57707dc759daaf864df4f972ebc2d0e9be9de1c8
yehjim <yeh19990402@gmail.com>
2020-02-28 11:07:47 +0800

    second

---
+++ src/components/MbSelect.vue
@@ -0,0 +1,212 @@
+<template>
+    <div class="container-fluid mbselect">
+        <span class="mbselecttitle">Mb Select</span>
+        <div class="row multiple-items">
+            <div class="selectitems">
+                <div class="itemsdetail">
+                    <div class="housetypeicon">
+                        <div class="iconbg">
+    
+                        </div>
+                    </div>
+                    <div class="detail">
+                        <div class="area">
+                            <span class="housearea">Taipei Zoghshan</span>
+                            <span class="housetype">Single room</span>
+                        </div>
+                        <div class="price">
+                            <span class="houseprice">NTD:12500</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="selectitems">
+                <div class="itemsdetail">
+                    <div class="housetypeicon">
+                        <div class="iconbg">
+    
+                        </div>
+                    </div>
+                    <div class="detail">
+                        <div class="area">
+                            <span class="housearea">Taipei Zoghshan</span>
+                            <span class="housetype">Single room</span>
+                        </div>
+                        <div class="price">
+                            <span class="houseprice">NTD:12500</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="selectitems">
+                <div class="itemsdetail">
+                    <div class="housetypeicon">
+                        <div class="iconbg">
+    
+                        </div>
+                    </div>
+                    <div class="detail">
+                        <div class="area">
+                            <span class="housearea">Taipei Zoghshan</span>
+                            <span class="housetype">Single room</span>
+                        </div>
+                        <div class="price">
+                            <span class="houseprice">NTD:12500</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="selectitems">
+                <div class="itemsdetail">
+                    <div class="housetypeicon">
+                        <div class="iconbg">
+    
+                        </div>
+                    </div>
+                    <div class="detail">
+                        <div class="area">
+                            <span class="housearea">Taipei Zoghshan</span>
+                            <span class="housetype">Single room</span>
+                        </div>
+                        <div class="price">
+                            <span class="houseprice">NTD:12500</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="selectitems">
+                <div class="itemsdetail">
+                    <div class="housetypeicon">
+                        <div class="iconbg">
+    
+                        </div>
+                    </div>
+                    <div class="detail">
+                        <div class="area">
+                            <span class="housearea">Taipei Zoghshan</span>
+                            <span class="housetype">Single room</span>
+                        </div>
+                        <div class="price">
+                            <span class="houseprice">NTD:12500</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script src="../src//assets//slick-1.8.1//slick//slick.js">
+
+</script>
+
+<script type="text/javascript" src="../src//assets//slick-1.8.1//slick//slick.min.js">
+
+</script>
+
+<script>
+export default {
+    name: 'MbSelect',
+    mounted() {
+        $(document).ready(function() {
+            $('.multiple-items').slick({
+                dots: true,
+                infinite: true,
+                slidesToShow: 3,
+                slidesToScroll: 1,
+                centerMode: true,
+                autoplay: true,
+                autoplaySpeed: 1000
+            });
+        });
+    },
+
+}
+</script>
+
+<style lang="scss" scoped>
+.mbselect {
+    width: 100%;
+    height: 600px;
+    margin-top: 50px;
+    .mbselecttitle {
+        font-size: 30px;
+        color: #666B46;
+        margin-left: 100px;
+    }
+    .slick-center {
+        transform: scale(1.2);
+    }
+    .multiple-items {
+        .selectitems {
+            background-image: url('../assets/media/patrick_perkins_3wylDrjxH_E_un_bx@2x.png');
+            background-position: center;
+            background-size: cover;
+            border-radius: 20px;
+            margin-right: 50px;
+            margin-left: 50px;
+            margin-top: 100px;
+            margin-bottom: 50px;
+            height: 300px;
+            display: flex;
+            justify-content: center;
+            align-items: flex-end;
+            box-sizing: border-box;
+            overflow: hidden;
+            .itemsdetail {
+                width: 200px;
+                height: 80px;
+                background-color: white;
+                margin-bottom: 50px;
+                border-radius: 17px;
+                box-shadow: 0 3px 6px rgba(0, 0, 0, 0.16), 0 3px 6px rgba(0, 0, 0, 0.23);
+                display: flex;
+                justify-content: center;
+                align-items: center;
+                box-sizing: border-box; // transform: scale(0);
+                transform: translateY(130px);
+                transition-duration: 0.5s;
+                .housetypeicon {
+                    width: 40%;
+                    height: 80px;
+                    display: flex;
+                    margin: auto;
+                    justify-content: center;
+                    align-items: center; // border: solid 1px;
+                    .iconbg {
+                        width: 60px;
+                        height: 60px;
+                        background-color: darkorange; // margin: auto;
+                        border-radius: 15px;
+                    }
+                }
+                .detail {
+                    width: 60%;
+                    height: 80px; // border: solid 1px;
+                    box-sizing: border-box;
+                    display: flex;
+                    flex-direction: column;
+                    justify-content: space-evenly;
+                    .housearea {
+                        font-size: 5px;
+                        color: #A6B6AE;
+                    }
+                    .housetype {
+                        font-size: 3px;
+                        color: #7AA08F;
+                    }
+                    .houseprice {
+                        font-size: 2px;
+                        color: #A6B6AE;
+                    }
+                }
+            }
+        }
+    }
+    .selectitems:hover {
+        .itemsdetail {
+            transform: translateY(10px);
+        }
+    }
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,6 +3,7 @@
     <MainPageTop></MainPageTop>
     <AboutMb></AboutMb>
     <AboutMb></AboutMb>
+    <MbSelect></MbSelect>
   </div>
 </template>
 
@@ -10,12 +11,14 @@
 // @ is an alias to /src
 import MainPageTop from '../components/MainPageTop'
 import AboutMb from '../components/AboutMB'
+import MbSelect from '../components/MbSelect'
 
 export default {
   name: 'Home',
   components: {
     MainPageTop,
-    AboutMb
+    AboutMb,
+    MbSelect
   }
 }
 </script>
