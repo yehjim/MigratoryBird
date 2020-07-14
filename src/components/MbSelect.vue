3186ff272eb68cff5cd70bc3a5d5f8b336b2248a
yehjim <yeh19990402@gmail.com>
2020-07-14 19:45:52 +0800

    mbselect更改完成，api串接

--- src/components/MbSelect.vue
+++ src/components/MbSelect.vue
@@ -2,96 +2,25 @@
     <div class="container-fluid mbselect">
         <span class="mbselecttitle">Mb Select</span>
         <div class="row multiple-items">
-            <div class="selectitems">
-                <div class="itemsdetail">
-                    <div class="housetypeicon">
-                        <div class="iconbg">
+            <div v-for="(item,index) in selectlist" :key="index">
+                <div class="selectitems">
+                    <div class="itemsdetail">
+                        <div class="housetypeicon">
+                            <div class="iconbg">
     
-                        </div>
-                    </div>
-                    <div class="detail">
-                        <router-link to="/HouseDetail">
-                            <div class="area">
-                                <span class="housearea">Taipei Zoghshan</span>
-                                <br>
-                                <span class="housetype">Single room</span>
                             </div>
-                            <div class="price">
-                                <span class="houseprice">NTD:12500</span>
-                            </div>
-                        </router-link>
-                    </div>
-                </div>
-            </div>
-            <div class="selectitems">
-                <div class="itemsdetail">
-                    <div class="housetypeicon">
-                        <div class="iconbg">
-    
                         </div>
-                    </div>
-                    <div class="detail">
-                        <div class="area">
-                            <span class="housearea">Taipei Zoghshan</span>
-                            <span class="housetype">Single room</span>
-                        </div>
-                        <div class="price">
-                            <span class="houseprice">NTD:12500</span>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="selectitems">
-                <div class="itemsdetail">
-                    <div class="housetypeicon">
-                        <div class="iconbg">
-    
-                        </div>
-                    </div>
-                    <div class="detail">
-                        <div class="area">
-                            <span class="housearea">Taipei Zoghshan</span>
-                            <span class="housetype">Single room</span>
-                        </div>
-                        <div class="price">
-                            <span class="houseprice">NTD:12500</span>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="selectitems">
-                <div class="itemsdetail">
-                    <div class="housetypeicon">
-                        <div class="iconbg">
-    
-                        </div>
-                    </div>
-                    <div class="detail">
-                        <div class="area">
-                            <span class="housearea">Taipei Zoghshan</span>
-                            <span class="housetype">Single room</span>
-                        </div>
-                        <div class="price">
-                            <span class="houseprice">NTD:12500</span>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="selectitems">
-                <div class="itemsdetail">
-                    <div class="housetypeicon">
-                        <div class="iconbg">
-    
-                        </div>
-                    </div>
-                    <div class="detail">
-                        <div class="area">
-                            <span class="housearea">Taipei Zoghshan</span>
-                            <br>
-                            <span class="housetype">Single room</span>
-                        </div>
-                        <div class="price">
-                            <span class="houseprice">NTD:12500</span>
+                        <div class="detail">
+                            <router-link to="/HouseDetail">
+                                <div class="area">
+                                    <span class="housearea">{{item.city}} {{item.area}}</span>
+                                    <br>
+                                    <span class="housetype">{{item.type}}</span>
+                                </div>
+                                <div class="price">
+                                    <span class="houseprice">NTD:{{item.monthly}}</span>
+                                </div>
+                            </router-link>
                         </div>
                     </div>
                 </div>
@@ -107,19 +36,30 @@
 </script>
 
 <script>
+import axios from 'axios'
 export default {
     name: 'MbSelect',
+    data() {
+        return {
+            selectlist: []
+        }
+    },
     mounted() {
         $(document).ready(function() {
             $('.multiple-items').slick({
                 infinite: true,
                 slidesToShow: 3,
                 slidesToScroll: 1,
-                centerMode: true,
-                // autoplay: true,
-                // autoplaySpeed: 1000
+                autoplay: true,
             });
         });
+        axios.get(`http://localhost:7000/housedetail?ad=true`)
+            .then((res) => {
+                this.selectlist = res.data
+            }).catch((err) => {
+                console.log(err)
+
+            })
     },
 
 }
@@ -134,22 +74,30 @@ export default {
         font-size: 30px;
         color: #666B46;
         margin-left: 100px;
-    }
-    .slick-center {
-        transition: 0.5s;
-        transform: scale(1.2);
-    }
+    } // .slick-current {
+    //     transition: 0.5s;
+    //     transform: scale(1.2);
+    // }
     .multiple-items {
+        // border: solid 1px;
+        display: flex;
+        justify-content: center;
+        height: 500px; // justify-content: center;
+        // width: 100%;
+        // border: solid 1px;
+        align-items: center;
         .selectitems {
+            // border: solid 1px;
             background-image: url('../assets/media/patrick_perkins_3wylDrjxH_E_un_bx@2x.png');
             background-position: center;
             background-size: cover;
             border-radius: 5px;
+            // margin-top: 100px;
+            // width: 33%;
+            width: 400px;
             margin-right: 50px;
             margin-left: 50px;
-            margin-top: 100px;
-            margin-bottom: 40px;
-            height: 300px;
+            height: 350px;
             display: flex;
             justify-content: center;
             align-items: flex-end;
@@ -158,7 +106,7 @@ export default {
             transition: 0.4s;
             box-shadow: 0 3px 6px rgba(0, 0, 0, 0.16), 0 3px 6px rgba(0, 0, 0, 0.23);
             .itemsdetail {
-                width: 250px;
+                width: 300px;
                 height: 85px;
                 background-color: white;
                 margin-bottom: 30px;
@@ -178,8 +126,8 @@ export default {
                     justify-content: center;
                     align-items: center; // border: solid 1px;
                     .iconbg {
-                        width: 50px;
-                        height: 50px;
+                        width: 60px;
+                        height: 60px;
                         background-color: darkorange; // margin: auto;
                         border-radius: 10px;
                     }
